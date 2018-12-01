b4ad0f795eb872ddac7f061ed11e5a99cd3bc47e
Gao Zhiyuan <alapha23@gmail.com>
2018-12-01 04:39:08 -0500

    For Mr. Cui to review

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
     //"github.com/davecgh/go-spew/spew"
 )
 
-func consume(batch int, start_pos []byte, topic string)pulsar.Message{
+func consume(batch int, topic string)pulsar.Message{
     // Instantiate a Pulsar client
     client, err := pulsar.NewClient(pulsar.ClientOptions{
             URL: "pulsar://localhost:6650",
@@ -22,12 +22,6 @@ func consume(batch int, start_pos []byte, topic string)pulsar.Message{
     if err != nil { log.Fatal(err) }
 
     // Use the client object to instantiate a consumer
-/*    consumer, err := client.Subscribe(pulsar.ConsumerOptions{
-        Topic:            topic,     // my-topic
-        SubscriptionName: "sub-1",
-    })
-  */
-    //fmt.Printf("%s\n", spew.sdump(pulsar.DeserializeMessageID(start_pos)))
     reader, err := client.CreateReader(pulsar.ReaderOptions{
         Topic:          topic,
         StartMessageID: pulsar.EarliestMessage,
@@ -37,7 +31,6 @@ func consume(batch int, start_pos []byte, topic string)pulsar.Message{
 
     if err != nil { log.Fatal(err) }
 
-    //defer consumer.Close()
     defer reader.Close()
 
     ctx := context.Background()
@@ -45,7 +38,6 @@ func consume(batch int, start_pos []byte, topic string)pulsar.Message{
     var msg pulsar.Message
     c1 := make(chan string, 1)
     go func() {
-        //msg, err = consumer.Receive(ctx)
         msg, err = reader.Next(ctx)
         if err != nil { log.Fatalf("Error reading from topic: %v", err) }
         c1 <- "Successly received a msg!\n"
@@ -71,21 +63,20 @@ func myhandler(w http.ResponseWriter, r *http.Request) {
     if ok != true { log.Fatal("Error parsing batch") }
     batch_i, err := strconv.Atoi(batch_s[0])
     if err != nil { log.Fatal(err) }
-    fmt.Fprintf(w, "Batch:%d ", batch_i);
 
-    start_pos_s, ok := r.URL.Query()["start_pos"]
+/*    start_pos_s, ok := r.URL.Query()["start_pos"]
     if ok != true { log.Fatal("Error parsing start_pos") }
-//    start_pos_i, err := strconv.Atoi(start_pos_s[0])
-//    if err != nil { log.Fatal(err) }
-//    fmt.Fprintf(w, "start_pos:%d ", start_pos_i);
-
+    start_pos_i, err := strconv.Atoi(start_pos_s[0])
+    if err != nil { log.Fatal(err) }
+    fmt.Fprintf(w, "start_pos:%d ", start_pos_i);
+*/
     topic, ok := r.URL.Query()["topic"]
     if ok != true { log.Fatal("Error parsing topic") }
-    fmt.Fprintf(w, "topic:%s ", topic[0]);
     // topic must be assigned
 
     for i:=0; i<batch_i; i++ {
-        msg := consume(batch_i, []byte(start_pos_s[0]), topic[0])
+        //LastSaveID := pulsar.DeserializeMessageID(pulsar.EarliestMessage)
+        msg := consume(batch_i, topic[0])
         if(msg!=nil) {
             fmt.Fprintf(w, string(msg.Payload()))
         }else {
@@ -98,16 +89,6 @@ func main() {
 
     mux := http.NewServeMux()
     mux.HandleFunc("/", myhandler)
-    /*func(w http.ResponseWriter, req *http.Request) {
-	fmt.Printf("%s\n", spew.sdump(req))
-        //deal_req(req);
-        msg := consume()
-	if(msg!="") {
-            fmt.Fprintf(w, msg.Payload())
-        }else {
-            fmt.Fprintf(w, "No log resides in pulsar\n")
-	}
-    })*/
 
     n := negroni.Classic() // Includes some default middlewares
     n.UseHandler(mux)
